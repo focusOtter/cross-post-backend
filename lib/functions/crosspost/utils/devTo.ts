4fc656aef2c8c04ee08733571f9b0aed0572ded1
focusotter <mtliendo@focusotter.com>
2023-07-27 13:13:19 -0500

    update with correct date --crosspost

--- lib/functions/crosspost/utils/devTo.ts
+++ lib/functions/crosspost/utils/devTo.ts
@@ -10,18 +10,18 @@ export const devToAPIKeyName = 'crosspost-devto-apikey'
 
 // this takes in the data.date field in frontmatter and converts it to the desired format
 
-function convertDateFormat(date: string) {
-	// Regular expression to match the "YYYY-MM-DD" format
-	const regex = /^\d{4}-\d{2}-\d{2}$/
-	const currentTime = new Date().toISOString().split('T')[1]
-	// If the input string matches the "YYYY-MM-DD" format
-	if (regex.test(date)) {
-		// '2023-07-27T17:58:57.117Z'
-		return `${date}T${currentTime}`
-	} else {
-		return date
-	}
-}
+// function convertDateFormat(date: string) {
+// 	// Regular expression to match the "YYYY-MM-DD" format
+// 	const regex = /^\d{4}-\d{2}-\d{2}$/
+// 	const currentTime = new Date().toISOString().split('T')[1]
+// 	// If the input string matches the "YYYY-MM-DD" format
+// 	if (regex.test(date)) {
+// 		// '2023-07-27T17:58:57.117Z'
+// 		return `${date}T${currentTime}`
+// 	} else {
+// 		return date
+// 	}
+// }
 
 function replaceShortcodesForDevTo(inputString: string) {
 	// Replace Twitter shortcodes with Dev.to embeds
@@ -52,18 +52,18 @@ export function prepForDevToPublishing(postWithFrontmatter: string): {
 	console.log('the dcontent', content)
 	console.log('the rrors', errors)
 	if (errors.length > 0) {
-		const devToDate = convertDateFormat(data.date)
-		console.log('the devToDate', devToDate)
-		const devToFrontmatter = {
-			...data,
-			date: devToDate,
-		}
+		// const devToDate = convertDateFormat(data.date)
+		// console.log('the devToDate', devToDate)
+		// const devToFrontmatter = {
+		// 	...data,
+		// 	date: devToDate,
+		// }
 
-		console.log('the devfrontmatter', devToFrontmatter)
+		console.log('the devfrontmatter', data)
 
 		const devToContent = replaceShortcodesForDevTo(content)
 
-		return { frontmatter: devToFrontmatter, content: devToContent }
+		return { frontmatter: data, content: devToContent }
 	} else {
 		console.log(errors)
 		throw new Error(errors)

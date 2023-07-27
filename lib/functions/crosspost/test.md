cf89757f6630327fecdcb539a5976ed55932f74a
focusotter <mtliendo@focusotter.com>
2023-07-27 13:22:48 -0500

    hoping to update --crosspost

--- lib/functions/crosspost/test.md
+++ lib/functions/crosspost/test.md
@@ -1,5 +1,5 @@
 ---
-title: Test markdown crosspost5
+title: Test markdown crosspost6
 description: I've been building APIs for years, but recently discovered the power of inferring data from user sessions for a better developer experience.
 date: 2023-07-27
 categories:
--- lib/functions/crosspost/utils/devTo.ts
+++ lib/functions/crosspost/utils/devTo.ts
@@ -49,11 +49,8 @@ export function prepForDevToPublishing(postWithFrontmatter: string): {
 } {
 	const { data, content, errors } = frontmatter(postWithFrontmatter)
 	console.log('the data', data)
-	console.log('the dcontent', content)
 	console.log('the rrors', errors)
 	if (errors.length > 0) {
-		// const devToDate = convertDateFormat(data.date)
-		// console.log('the devToDate', devToDate)
 		const devToFrontmatter = {
 			...data,
 			date: `${data.date}`,
